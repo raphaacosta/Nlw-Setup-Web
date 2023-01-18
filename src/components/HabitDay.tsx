077e7b8c13fafcec85ca19982cc77876f7972469
Raphael Costa <rafa.costa0@hotmail.com>
2023-01-18 19:46:58 -0300

    add visual components

---
+++ src/components/HabitDay.tsx
@@ -0,0 +1,13 @@
+// interface HabitProps {
+//   completed: number;
+// }
+
+const HabitDay = (/*props: HabitProps*/) => {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
+
+    </div>
+  )
+}
+
+export default HabitDay;
