eede929af974b4751d6bf306c0bb2e4288ce4170
Raphael Costa <rafa.costa0@hotmail.com>
2023-01-18 19:46:46 -0300

    add tawilwindcss to html, change App import in main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import Habit from './components/Habit'
-import './styles/global.css'
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
+import './styles/global.css';
 
-function App() {
+export const App = () => {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={10} />
-      <Habit completed={15} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
 
-export default App
