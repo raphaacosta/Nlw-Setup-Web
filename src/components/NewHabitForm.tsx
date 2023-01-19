b9ac93f073bfccc4726f14ca558444f2a2ac45f4
Raphael Costa <rafa.costa0@hotmail.com>
2023-01-19 20:20:53 -0300

    first connection with api

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,8 +1,52 @@
+import { FormEvent, useState } from "react";
 import { Check } from "phosphor-react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import api from "../lib/axios";
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado',
+];
 
 const NewHabitForm = () => {
+  const [title, setTitle] = useState('');
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  const createNewHabit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!title || weekDays.length === 0) return;
+
+    await api.post('habits', {
+      title,
+      weekDays,
+    }).then(() => {
+      alert('Hábito criado com sucesso')
+      setTitle('')
+      setWeekDays([])
+    })
+  }
+
+  const handleToggleWeekDay = (weekDay: number) => {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay);
+
+      setWeekDays(weekDaysWithRemovedOne);
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay]
+
+      setWeekDays(weekDaysWithAddedOne);
+    }
+  }
+
   return (
     <form
+      onSubmit={createNewHabit}
       className="w-full flex flex-col mt-6"
     >
       <label htmlFor="title" className="font-semibold leading-tight">
@@ -15,11 +59,38 @@ const NewHabitForm = () => {
         id="title"
         placeholder="ex.: Exercícios, dormir bem, etc..."
         autoFocus
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
       />
 
       <label className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox.Root
+            key={weekDay}
+            className="flex items-center gap-3 group"
+            onCheckedChange={() => handleToggleWeekDay(index)}
+            checked={weekDays.includes(index)}
+          >
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check
+                  size={20}
+                  className='text-white'
+                />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className="text-white leading-tight">
+              {weekDay}
+            </span>
+          </Checkbox.Root>
+        ))}
+
+      </div>
 
       <button
         type="submit"
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,5 +1,8 @@
-import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
-import HabitDay from "./HabitDay"
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import HabitDay from "./HabitDay";
+import api from "../lib/axios";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 
 const weekDays = [
   'D',
@@ -16,7 +19,27 @@ const summaryDates = generateDatesFromYearBeginning();
 const minimumSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
+type Summary = Array<{
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}>
+
 export const SummaryTable = () => {
+  const [summary, setSummary] = useState<Summary>([]);
+
+  useEffect(() => {
+    getSummaries()
+  }, [])
+
+  const getSummaries = async () => {
+    await api.get('summary')
+      .then(response => {
+        setSummary(response.data);
+      })
+  }
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -28,13 +51,19 @@ export const SummaryTable = () => {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => (
-          (<HabitDay
-            key={date.toString()}
-            amount={5}
-            completed={Math.round(Math.random() * 5)}
-          />)
-        ))}
+        {summaryDates.map(date => {
+          const dayInSummary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day')
+          })
+          return (
+            <HabitDay
+              key={date.toString()}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
+            />
+          )
+        })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return (
