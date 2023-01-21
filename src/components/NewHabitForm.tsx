5046544cdaba742698e92694bf8894ec101b6d9c
Raphael Costa <rafa.costa0@hotmail.com>
2023-01-21 20:59:02 -0300

    add animation in progressbar and checkbox, add accessibility to tab functionality

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -54,7 +54,7 @@ const NewHabitForm = () => {
       </label>
 
       <input
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900"
         type="text"
         id="title"
         placeholder="ex.: Exercícios, dormir bem, etc..."
@@ -67,15 +67,15 @@ const NewHabitForm = () => {
         Qual a recorrência?
       </label>
 
-      <div className="flex flex-col gap-2 mt-3">
+      <div className="flex flex-col gap-2 mt-3 transition-colors">
         {availableWeekDays.map((weekDay, index) => (
           <Checkbox.Root
             key={weekDay}
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none"
             onCheckedChange={() => handleToggleWeekDay(index)}
             checked={weekDays.includes(index)}
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background">
               <Checkbox.Indicator>
                 <Check
                   size={20}
@@ -94,7 +94,7 @@ const NewHabitForm = () => {
 
       <button
         type="submit"
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirmar
